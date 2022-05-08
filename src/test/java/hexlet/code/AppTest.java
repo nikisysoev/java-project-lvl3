7b54d007103e4e86a64c22e6a5a81b951c9bb2b0
Nikita Sysoev <nikisysoev@gmail.com>
2022-05-08 17:49:55 +0700

    gradle init

---
+++ src/test/java/hexlet/code/AppTest.java
@@ -0,0 +1,11 @@
+package hexlet.code;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class AppTest {
+    @Test
+    void helloTest() {
+        assertEquals("Hello", App.Hello());
+    }
+}
